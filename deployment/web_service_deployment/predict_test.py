8ed0b8f572a4c7053945c6ee8d7a3bd0f3f98225
Turtle344 <khanthtoonaing2000@gmail.com>
2024-07-28 14:54:07 +0700

    minor fix

--- deployment/web_service_deployment/predict_test.py
+++ deployment/web_service_deployment/predict_test.py
@@ -1,6 +1,5 @@
 import requests
 
-# Test case 1: Successful campaign
 test_data = {
   "goalamount": 76159,
   "raisedamount": 73518.02895458053,
@@ -17,4 +16,4 @@ test_data = {
 }
 
 response = requests.post("http://localhost:8000/predict", json=test_data)
-print(f"Test case 1 (Successful campaign): {response.json()}")
+print(f"Test case 1 Campaign Success Prediction: {response.json()}")
